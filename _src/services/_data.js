74f6772891ebe70186cadc7b839b0a5efc30d43e
tjaynl <mail@tjay.nl>
2025-03-09 13:27:49 +0100

    updated the website

--- _src/services/_data.js
+++
@@ -1,9 +0,0 @@
-export const basename = "services";
-export const type = "service";
-export const layout = "layouts/service.vto";
-export const templateEngine = "vto,md";
-export const description = "Onze diensten";
-
-export function url(page) {
-    return `/services/${page.data.basename}/`;
-}
